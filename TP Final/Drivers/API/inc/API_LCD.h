aa1b2d1b176c93a5e7d7550ff7a2acddf88a9c55
Lucas C <lconstantino@skyloom.co>
2022-04-20 04:17:35 -0300

    construccion driver LCD

--- TP Final/Drivers/API/inc/API_LCD.h
+++ TP Final/Drivers/API/inc/API_LCD.h
@@ -11,6 +11,21 @@
 #include <stdbool.h>
 #include "stm32f4xx_hal.h"  		/* <- HAL include */
 
+#define LCD_SCREEN_HEIGHT 	240
+#define LCD_SCREEN_WIDTH 	320
+
+//RESET PIN AND PORT, STANDARD GPIO
+#define	LCD_RST_PORT		GPIOC
+#define	LCD_RST_PIN			GPIO_PIN_10
+
+//CS PIN AND PORT, STANDARD GPIO
+#define	LCD_DC_PORT			GPIOC
+#define	LCD_DC_PIN			GPIO_PIN_12
+
+//DC PIN AND PORT, STANDARD GPIO
+#define	LCD_CS_PORT			GPIOC
+#define	LCD_CS_PIN			GPIO_PIN_14
+
 typedef struct
 {
 	SPI_HandleTypeDef SPI_Handle;
@@ -18,8 +33,17 @@ typedef struct
 	uint16_t LCDPixelSizeY;
 }LCDHandle_t;
 
-// Inicializa el handle del LCD
-void LCD_init (void);
+// Configura los pines del LCD
+void LCD_Config(void);
+
+// Inicializa el LCD
+void LCD_Init(void);
+
+void LCD_Write_Command(uint8_t Command);
+
+void LCD_Write_Data(uint8_t Data);
+
+void LCD_SPI_Send(unsigned char SPI_Data);
 
 // Escribe un texto sobre la pantalla en la posición indicada
 //void LCD_writeText(const char * str, color, position, size);
@@ -29,5 +53,7 @@ void LCD_init (void);
 
 //
 void LCD_refresh(void);
+
+//void LCD_Error_Handler(void);
 
 #endif /* API_INC_API_LCD_H_ */
